7e63087b99461aeb51928f78388d15d4fb13c692
Jeremy Petty <91158393+jp3tty@users.noreply.github.com>
2025-12-11 09:10:21 -0800

    Updating ticker list

--- pull_stock_candles.py
+++ pull_stock_candles.py
@@ -68,7 +68,7 @@ def main():
         symbol_list = df['Ticker'].tolist()
         
         # Hardcoding additional tickers (to monitor specific stocks)
-        additional_tickers = ['INTC', 'SB', 'AMAT', 'AAPL', 'ALMS', 'FSMD', 'KOS']
+        additional_tickers = ['INTC', 'SB', 'AMAT', 'AAPL', 'ALMS', 'FSMD']
         symbol_list = list(set(symbol_list + additional_tickers))
         
         logging.info(f"Processing {len(symbol_list)} tickers")
